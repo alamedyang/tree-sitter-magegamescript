e7c66b1d5edb1b2233e5d18de5f6794c97d6da31
Alamedyang <alamedyang@gmail.com>
2025-05-03 13:24:40 -0600

    functions go inside

--- parser-utilities/file-state.js
+++ parser-utilities/file-state.js
@@ -13,47 +13,50 @@ const makeFileState = (fileName, parser) => {
 		warnings: [],
 		nodes: [],
 		parser,
-	};
-	const newWarningOrError = (location, message, type) => {
-		const locations = Array.isArray(location)
-			? location
-			: [ location ];
-		locations.forEach(item=>{
-			if (!item.fileName) item.fileName = f.fileName;
-		})
-		f[type].push({ message, locations });
-	}
-	f.newWarning = (location, message) => newWarningOrError(location, message, 'warnings');
-	f.newError = (location, message) => newWarningOrError(location, message, 'errors');
-	f.mergeF = (newF) => {
-		Object.keys(newF.constants).forEach(constantName=>{
-			if (f.constants[constantName]) {
-				f.newError(
-					{
-						node: newF.constants[constantName].node, 
-						fileName: newF.fileName,
-					},
-					`cannot redefine constant ${constantName} (via 'include_macro')`
-				);
-			}
-			f.constants[constantName] = newF.constants[constantName];
-		});
-		newF.nodes.forEach(node=>{ f.nodes.push(node) });
-		['default', 'serial'].forEach(type=>{
-			Object.keys(newF.settings[type]).forEach(param=>{
-				f.settings[type][param] = newF.settings[type][param];
+		newWarning: (location, message) => {
+			const locations = Array.isArray(location) ? location : [ location ];
+			locations.forEach(item=>{
+				if (!item.fileName) item.fileName = f.fileName;
+			})
+			f.warnings.push({ message, locations });
+		},
+		newWarning: (location, message) => {
+			const locations = Array.isArray(location) ? location : [ location ];
+			locations.forEach(item=>{
+				if (!item.fileName) item.fileName = f.fileName;
+			})
+			f.errors.push({ message, locations });
+		},
+		mergeF: (newF) => {
+			Object.keys(newF.constants).forEach(constantName=>{
+				if (f.constants[constantName]) {
+					f.newError(
+						{
+							node: newF.constants[constantName].node, 
+							fileName: newF.fileName,
+						},
+						`cannot redefine constant ${constantName} (via 'include_macro')`
+					);
+				}
+				f.constants[constantName] = newF.constants[constantName];
 			});
-		});
-		['entity', 'label'].forEach(type=>{
-			Object.keys(newF.settings[type]).forEach(target=>{
-				const params = Object.keys(newF.settings[type][target]);
-				f.settings[type][target] = f.settings[type][target] || {};
-				params.forEach(param=>{
-					f.settings[type][target][param] = newF.settings[type][target][param];
+			newF.nodes.forEach(node=>{ f.nodes.push(node) });
+			['default', 'serial'].forEach(type=>{
+				Object.keys(newF.settings[type]).forEach(param=>{
+					f.settings[type][param] = newF.settings[type][param];
 				});
 			});
-		});
-	}
+			['entity', 'label'].forEach(type=>{
+				Object.keys(newF.settings[type]).forEach(target=>{
+					const params = Object.keys(newF.settings[type][target]);
+					f.settings[type][target] = f.settings[type][target] || {};
+					params.forEach(param=>{
+						f.settings[type][target][param] = newF.settings[type][target][param];
+					});
+				});
+			});
+		},
+	};
 	return f;
 };
 
--- parser-utilities/project-state.js
+++ parser-utilities/project-state.js
@@ -7,68 +7,68 @@ const makeProjectState = () => {
 		scripts: {},
 		errors: [],
 		warnings: [],
-	};
-	p.addScript = (data, fileName) => {
-		const scriptName = data.scriptName;
-		data.rawActions = data.actions;
-		data.actions = finalizeActions(p, data.actions, fileName);
-		if (!p.scripts[scriptName]) {
-			p.scripts[scriptName] = data;
-		} else {
-			if (!p.scripts[scriptName].duplicates) {
-				p.scripts[scriptName].duplicates = [ p.scripts[scriptName] ];
-			}
-			p.scripts[scriptName].duplicates.push(data);
-		}
-	}
-	p.addDialog = (data, fileName) => {
-		const dialogName = data.dialogName;
-		if (!p.dialogs[dialogName]) {
-			p.dialogs[dialogName] = data;
-		} else {
-			if (!p.dialogs[dialogName].duplicates) {
-				p.dialogs[dialogName].duplicates = [ p.dialogs[dialogName] ];
-			}
-			p.dialogs[dialogName].duplicates.push(data);
-		}
-	};
-	p.addSerialDialog = (data, fileName) => {
-		const serialDialogName = data.serialDialogName;
-		if (!p.dialogs[serialDialogName]) {
-			p.dialogs[serialDialogName] = data;
-		} else {
-			if (!p.dialogs[serialDialogName].duplicates) {
-				p.dialogs[serialDialogName].duplicates = [ p.dialogs[serialDialogName] ];
-			}
-			p.dialogs[serialDialogName].duplicates.push(data);
-		}
-	};
-	p.detectDuplicates = () => {
-		[ 'scripts', 'dialogs', 'serialDialogs' ].forEach(category=>{
-			const entries = Object.entries(p[category]);
-			entries.forEach(([name, entry])=>{
-				if (entry.duplicates) {
-					p.errors.push({
-						locations: entry.duplicates.map(dupe=>({
-							fileName: dupe.fileName,
-							// TODO: work this part out better
-							node: dupe.debug.firstNamedChild,
-						})),
-						message: `multiple ${category} with name "${name}"`,
-					});
+		addScript: (data, fileName) => {
+			const scriptName = data.scriptName;
+			data.rawActions = data.actions;
+			data.actions = finalizeActions(p, data.actions, fileName);
+			if (!p.scripts[scriptName]) {
+				p.scripts[scriptName] = data;
+			} else {
+				if (!p.scripts[scriptName].duplicates) {
+					p.scripts[scriptName].duplicates = [ p.scripts[scriptName] ];
 				}
+				p.scripts[scriptName].duplicates.push(data);
+			}
+		},
+		addDialog: (data, fileName) => {
+			const dialogName = data.dialogName;
+			if (!p.dialogs[dialogName]) {
+				p.dialogs[dialogName] = data;
+			} else {
+				if (!p.dialogs[dialogName].duplicates) {
+					p.dialogs[dialogName].duplicates = [ p.dialogs[dialogName] ];
+				}
+				p.dialogs[dialogName].duplicates.push(data);
+			}
+		},
+		addSerialDialog: (data, fileName) => {
+			const serialDialogName = data.serialDialogName;
+			if (!p.dialogs[serialDialogName]) {
+				p.dialogs[serialDialogName] = data;
+			} else {
+				if (!p.dialogs[serialDialogName].duplicates) {
+					p.dialogs[serialDialogName].duplicates = [ p.dialogs[serialDialogName] ];
+				}
+				p.dialogs[serialDialogName].duplicates.push(data);
+			}
+		},
+		detectDuplicates: () => {
+			[ 'scripts', 'dialogs', 'serialDialogs' ].forEach(category=>{
+				const entries = Object.entries(p[category]);
+				entries.forEach(([name, entry])=>{
+					if (entry.duplicates) {
+						p.errors.push({
+							locations: entry.duplicates.map(dupe=>({
+								fileName: dupe.fileName,
+								// TODO: work this part out better
+								node: dupe.debug.firstNamedChild,
+							})),
+							message: `multiple ${category} with name "${name}"`,
+						});
+					}
+				});
 			});
-		});
-	};
-	p.reportProblems = (fileMap) => {
-		p.warnings.forEach(v=>{
-			const s = makeMessagePrintable(fileMap, 'Warning', v);
-			console.warn(s);
-		})
-		p.errors.forEach(v=>{
-			const s = makeMessagePrintable(fileMap, 'Error', v);
-			console.error(s);
-		})
+		},
+		reportProblems: (fileMap) => {
+			p.warnings.forEach(v=>{
+				const s = makeMessagePrintable(fileMap, 'Warning', v);
+				console.warn(s);
+			})
+			p.errors.forEach(v=>{
+				const s = makeMessagePrintable(fileMap, 'Error', v);
+				console.error(s);
+			})
+		},
 	};
 	return p;
 };
